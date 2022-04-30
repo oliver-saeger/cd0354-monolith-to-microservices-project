da99877405551d8734e85a3e08e88d865dec85e4
Oliver Saeger <saeger.oliver@gmail.com>
2022-04-30 16:42:51 +0200

    add deployments and screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aea006789ba62416782c565c4f851ab8-230946438.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
